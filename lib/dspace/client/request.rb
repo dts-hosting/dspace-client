892f9476e245d95d37209c01791f9e46bf731a1f
Mark Cooper <mark.c.cooper@outlook.com>
2022-06-28 08:00:14 -0700

    Pre re-org

---
+++ lib/dspace/client/request.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+
+# DSpace
+module DSpace
+  # Request
+  module Request
+    def get(path, params = {}, headers = {})
+      response = connection.get(path) do |req|
+        req.params  = params
+        req.headers = headers.merge({ "X-XSRF-Token" => @token })
+        req.headers["Authorization"] = @authorization if @authorization
+      end
+      refresh_token(response)
+    end
+
+    def post(path, payload, params = {}, headers = {})
+      response = connection.post(path) do |req|
+        req.body    = payload.to_json
+        req.params  = params
+        req.headers = headers.merge({ "Authorization" => @authorization, "X-XSRF-Token" => @token })
+      end
+      refresh_token(response)
+    end
+
+    private
+
+    def refresh_authorization(response)
+      @authorization = response.headers["Authorization"] if response.headers.key?("Authorization")
+      response
+    end
+
+    def refresh_token(response)
+      @token = response.headers["DSPACE-XSRF-TOKEN"] if response.headers.key?("DSPACE-XSRF-TOKEN")
+      response
+    end
+  end
+end

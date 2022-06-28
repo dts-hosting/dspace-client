5011262b271a486f9e4c2931ee0fcd1ecec0e21e
Mark Cooper <mark.c.cooper@outlook.com>
2022-06-27 20:01:04 -0700

    Cop

--- examples/dspace.rb
+++ examples/dspace.rb
@@ -1,11 +1,13 @@
-#!/usr/bin/ruby
 # frozen_string_literal: true
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "dspace/client"
 
-config = DSpace::Configuration.new(settings: { rest_url: "https://demo.dspacedirect.net/server/api", username: "demo@dspacedirect.org",
-                                               password: "direct!" })
+config = DSpace::Configuration.new(settings: {
+                                     rest_url: "https://demo.dspacedirect.net/server/api",
+                                     username: "demo@dspacedirect.org",
+                                     password: "direct!"
+                                   })
 client = DSpace::Client.new(config: config)
 puts client.login
 puts client.get("core/items").body
